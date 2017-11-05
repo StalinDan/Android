3c730b0cb142cac0ee361e7b98e2d4c8e4229bef
殷会丹 <285954980@qq.com>
2017-11-05 23:39:24 +0800

    Activity

--- ActivityTest/app/src/main/java/com/example/danish/activitytest/ThirdActivity.java
+++ ActivityTest/app/src/main/java/com/example/danish/activitytest/ThirdActivity.java
@@ -2,12 +2,25 @@ package com.example.danish.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class ThirdActivity extends AppCompatActivity {
+public class ThirdActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third_layout);
+
+        Log.d("ThirdActivity","Task id is" + getTaskId());
+
+        Button btn3 = findViewById(R.id.button_3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCollector.finishAll();
+            }
+        });
     }
 }
