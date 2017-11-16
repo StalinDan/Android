36e062243ac7e606a2eb0cb104bef0b71195c981
Danish <285954980@qq.com>
2017-11-16 15:43:59 +0800

    数据存储

--- BroadcastBestPractice/app/src/main/java/com/example/danish/broadcastbestpractice/LoginActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/danish/broadcastbestpractice/LoginActivity.java
@@ -2,10 +2,14 @@ package com.example.danish.broadcastbestpractice;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,6 +18,10 @@ public class LoginActivity extends BaseActivity {
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
+
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +32,38 @@ public class LoginActivity extends BaseActivity {
         passwordEdit = findViewById(R.id.password);
         login = findViewById(R.id.login);
 
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        checkBox = findViewById(R.id.remeber_pass);
+        final boolean isRemember = pref.getBoolean("remember_password",false);
+
+        if (isRemember) {
+            String count = pref.getString("account","");
+            String password = pref.getString("password","");
+            accountEdit.setText(count);
+            passwordEdit.setText(password);
+            checkBox.setChecked(true);
+        }
+
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String account = accountEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
                 if (account.equals("admin")   && password.equals("123456")) {
+
+                    editor = pref.edit();
+                    if (checkBox.isChecked()) {
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    } else {
+                        editor.clear();
+                    }
+                    editor.apply();
+
+
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
---
+++ FilePersistenceTest/app/src/main/java/com/example/danish/filepersistencetest/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.danish.filepersistencetest;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edit = findViewById(R.id.edit);
+        String inputText = load();
+        if (!TextUtils.isEmpty(inputText)) {
+            edit.setText(inputText);
+            edit.setSelection(inputText.length());
+            Toast.makeText(this,"存储成功",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String inputStr = edit.getText().toString();
+        save(inputStr);
+    }
+
+    public void save (String inputStr) {
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+
+        try {
+            out = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputStr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String load() {
+        FileInputStream inputStream = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+
+        try {
+            inputStream = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();;
+        }
+        return content.toString();
+    }
+}
