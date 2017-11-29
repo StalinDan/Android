edac26982cc82e8dbbe4e2653a3fd726e9f54f01
Danish <285954980@qq.com>
2017-11-29 18:07:59 +0800

    Demo

---
+++ FragmentBottomBar/app/src/main/java/com/example/danish/fragmentbottombar/widget/SelectionBar.java
@@ -0,0 +1,17 @@
+package com.example.danish.fragmentbottombar.widget;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+/**
+ * Created by danish on 2017/11/29.
+ */
+
+public class SelectionBar extends LinearLayout {
+
+private
+
+    public SelectionBar(Context context) {
+        super(context);
+    }
+}
